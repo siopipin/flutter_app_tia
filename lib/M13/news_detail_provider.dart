9772acd2f0eab446d65f48762fc330a06dd4d161
siopipin <siojurnalispipin@hotmail.com>
2025-06-24 14:26:55 +0700

    getnewDetail

--- lib/M13/news_detail_provider.dart
+++ lib/M13/news_detail_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/M13/news_detail_model.dart';
+import 'package:http/http.dart' as http;
 
 class NewsDetailProvider extends ChangeNotifier {
   NewsDetailModel _newsDetailModel = NewsDetailModel();
@@ -7,5 +8,18 @@ class NewsDetailProvider extends ChangeNotifier {
   set setDataDetailBerita(val) {
     _newsDetailModel = val;
     notifyListeners();
+  }
+
+  getNewsDetail(String id) async {
+    try {
+      final url =
+          'https://7a26-2404-c0-1c20-00-98a-183.ngrok-free.app/detail/$id';
+
+      var response = await http.get(
+        Uri.parse(url),
+        headers: {'ngrok-skip-browser-warning': 'any'},
+      );
+    } catch (e) {
+    } finally {}
   }
 }
